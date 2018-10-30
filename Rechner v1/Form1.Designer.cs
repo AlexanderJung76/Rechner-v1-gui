63b4da793dd496e33cfc1533b864d788529c1360
Alexander Jung <alexanderj@gmx.net>
2018-10-30 13:14:28 +0100

    Projektdateien hinzufügen.

---
+++ Rechner v1/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Rechner_v1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RTB_Ausgabe_Fenster = new System.Windows.Forms.RichTextBox();
+            this.BTN_Plus = new System.Windows.Forms.Button();
+            this.BTN_Minus = new System.Windows.Forms.Button();
+            this.BTN_Multiply = new System.Windows.Forms.Button();
+            this.BTN_Division = new System.Windows.Forms.Button();
+            this.BTN_1 = new System.Windows.Forms.Button();
+            this.BTN_2 = new System.Windows.Forms.Button();
+            this.BTN_3 = new System.Windows.Forms.Button();
+            this.BTN_4 = new System.Windows.Forms.Button();
+            this.BTN_5 = new System.Windows.Forms.Button();
+            this.BTN_6 = new System.Windows.Forms.Button();
+            this.BTN_7 = new System.Windows.Forms.Button();
+            this.BTN_8 = new System.Windows.Forms.Button();
+            this.BTN_9 = new System.Windows.Forms.Button();
+            this.BTN_0 = new System.Windows.Forms.Button();
+            this.BTN_Comma = new System.Windows.Forms.Button();
+            this.BTN_Ergebnis = new System.Windows.Forms.Button();
+            this.BTN_Clear = new System.Windows.Forms.Button();
+            this.BTN_Sqrt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RTB_Ausgabe_Fenster
+            // 
+            this.RTB_Ausgabe_Fenster.Location = new System.Drawing.Point(12, 12);
+            this.RTB_Ausgabe_Fenster.Name = "RTB_Ausgabe_Fenster";
+            this.RTB_Ausgabe_Fenster.Size = new System.Drawing.Size(302, 81);
+            this.RTB_Ausgabe_Fenster.TabIndex = 0;
+            this.RTB_Ausgabe_Fenster.Text = "";
+            // 
+            // BTN_Plus
+            // 
+            this.BTN_Plus.Location = new System.Drawing.Point(264, 146);
+            this.BTN_Plus.Name = "BTN_Plus";
+            this.BTN_Plus.Size = new System.Drawing.Size(50, 50);
+            this.BTN_Plus.TabIndex = 1;
+            this.BTN_Plus.Text = "+";
+            this.BTN_Plus.UseVisualStyleBackColor = true;
+            this.BTN_Plus.Click += new System.EventHandler(this.BTN_Plus_Click);
+            // 
+            // BTN_Minus
+            // 
+            this.BTN_Minus.Location = new System.Drawing.Point(264, 202);
+            this.BTN_Minus.Name = "BTN_Minus";
+            this.BTN_Minus.Size = new System.Drawing.Size(50, 50);
+            this.BTN_Minus.TabIndex = 2;
+            this.BTN_Minus.Text = "-";
+            this.BTN_Minus.UseVisualStyleBackColor = true;
+            this.BTN_Minus.Click += new System.EventHandler(this.BTN_Minus_Click);
+            // 
+            // BTN_Multiply
+            // 
+            this.BTN_Multiply.Location = new System.Drawing.Point(264, 258);
+            this.BTN_Multiply.Name = "BTN_Multiply";
+            this.BTN_Multiply.Size = new System.Drawing.Size(50, 50);
+            this.BTN_Multiply.TabIndex = 3;
+            this.BTN_Multiply.Text = "*";
+            this.BTN_Multiply.UseVisualStyleBackColor = true;
+            this.BTN_Multiply.Click += new System.EventHandler(this.BTN_Multiply_Click);
+            // 
+            // BTN_Division
+            // 
+            this.BTN_Division.Location = new System.Drawing.Point(264, 314);
+            this.BTN_Division.Name = "BTN_Division";
+            this.BTN_Division.Size = new System.Drawing.Size(50, 50);
+            this.BTN_Division.TabIndex = 4;
+            this.BTN_Division.Text = "/";
+            this.BTN_Division.UseVisualStyleBackColor = true;
+            this.BTN_Division.Click += new System.EventHandler(this.BTN_Division_Click);
+            // 
+            // BTN_1
+            // 
+            this.BTN_1.Location = new System.Drawing.Point(12, 146);
+            this.BTN_1.Name = "BTN_1";
+            this.BTN_1.Size = new System.Drawing.Size(50, 50);
+            this.BTN_1.TabIndex = 5;
+            this.BTN_1.Text = "1";
+            this.BTN_1.UseVisualStyleBackColor = true;
+            this.BTN_1.Click += new System.EventHandler(this.BTN_1_Click);
+            // 
+            // BTN_2
+            // 
+            this.BTN_2.Location = new System.Drawing.Point(68, 146);
+            this.BTN_2.Name = "BTN_2";
+            this.BTN_2.Size = new System.Drawing.Size(50, 50);
+            this.BTN_2.TabIndex = 6;
+            this.BTN_2.Text = "2";
+            this.BTN_2.UseVisualStyleBackColor = true;
+            this.BTN_2.Click += new System.EventHandler(this.BTN_2_Click);
+            // 
+            // BTN_3
+            // 
+            this.BTN_3.Location = new System.Drawing.Point(124, 146);
+            this.BTN_3.Name = "BTN_3";
+            this.BTN_3.Size = new System.Drawing.Size(50, 50);
+            this.BTN_3.TabIndex = 7;
+            this.BTN_3.Text = "3";
+            this.BTN_3.UseVisualStyleBackColor = true;
+            this.BTN_3.Click += new System.EventHandler(this.BTN_3_Click);
+            // 
+            // BTN_4
+            // 
+            this.BTN_4.Location = new System.Drawing.Point(12, 202);
+            this.BTN_4.Name = "BTN_4";
+            this.BTN_4.Size = new System.Drawing.Size(50, 50);
+            this.BTN_4.TabIndex = 8;
+            this.BTN_4.Text = "4";
+            this.BTN_4.UseVisualStyleBackColor = true;
+            this.BTN_4.Click += new System.EventHandler(this.BTN_4_Click);
+            // 
+            // BTN_5
+            // 
+            this.BTN_5.Location = new System.Drawing.Point(68, 202);
+            this.BTN_5.Name = "BTN_5";
+            this.BTN_5.Size = new System.Drawing.Size(50, 50);
+            this.BTN_5.TabIndex = 9;
+            this.BTN_5.Text = "5";
+            this.BTN_5.UseVisualStyleBackColor = true;
+            this.BTN_5.Click += new System.EventHandler(this.BTN_5_Click);
+            // 
+            // BTN_6
+            // 
+            this.BTN_6.Location = new System.Drawing.Point(124, 202);
+            this.BTN_6.Name = "BTN_6";
+            this.BTN_6.Size = new System.Drawing.Size(50, 50);
+            this.BTN_6.TabIndex = 10;
+            this.BTN_6.Text = "6";
+            this.BTN_6.UseVisualStyleBackColor = true;
+            this.BTN_6.Click += new System.EventHandler(this.BTN_6_Click);
+            // 
+            // BTN_7
+            // 
+            this.BTN_7.Location = new System.Drawing.Point(12, 258);
+            this.BTN_7.Name = "BTN_7";
+            this.BTN_7.Size = new System.Drawing.Size(50, 50);
+            this.BTN_7.TabIndex = 11;
+            this.BTN_7.Text = "7";
+            this.BTN_7.UseVisualStyleBackColor = true;
+            this.BTN_7.Click += new System.EventHandler(this.BTN_7_Click);
+            // 
+            // BTN_8
+            // 
+            this.BTN_8.Location = new System.Drawing.Point(68, 258);
+            this.BTN_8.Name = "BTN_8";
+            this.BTN_8.Size = new System.Drawing.Size(50, 50);
+            this.BTN_8.TabIndex = 12;
+            this.BTN_8.Text = "8";
+            this.BTN_8.UseVisualStyleBackColor = true;
+            this.BTN_8.Click += new System.EventHandler(this.BTN_8_Click);
+            // 
+            // BTN_9
+            // 
+            this.BTN_9.Location = new System.Drawing.Point(124, 258);
+            this.BTN_9.Name = "BTN_9";
+            this.BTN_9.Size = new System.Drawing.Size(50, 50);
+            this.BTN_9.TabIndex = 13;
+            this.BTN_9.Text = "9";
+            this.BTN_9.UseVisualStyleBackColor = true;
+            this.BTN_9.Click += new System.EventHandler(this.BTN_9_Click);
+            // 
+            // BTN_0
+            // 
+            this.BTN_0.Location = new System.Drawing.Point(12, 314);
+            this.BTN_0.Name = "BTN_0";
+            this.BTN_0.Size = new System.Drawing.Size(106, 50);
+            this.BTN_0.TabIndex = 14;
+            this.BTN_0.Text = "0";
+            this.BTN_0.UseVisualStyleBackColor = true;
+            this.BTN_0.Click += new System.EventHandler(this.BTN_0_Click);
+            // 
+            // BTN_Comma
+            // 
+            this.BTN_Comma.Location = new System.Drawing.Point(124, 314);
+            this.BTN_Comma.Name = "BTN_Comma";
+            this.BTN_Comma.Size = new System.Drawing.Size(50, 50);
+            this.BTN_Comma.TabIndex = 15;
+            this.BTN_Comma.Text = ",";
+            this.BTN_Comma.UseVisualStyleBackColor = true;
+            this.BTN_Comma.Click += new System.EventHandler(this.BTN_Comma_Click);
+            // 
+            // BTN_Ergebnis
+            // 
+            this.BTN_Ergebnis.Location = new System.Drawing.Point(208, 258);
+            this.BTN_Ergebnis.Name = "BTN_Ergebnis";
+            this.BTN_Ergebnis.Size = new System.Drawing.Size(50, 106);
+            this.BTN_Ergebnis.TabIndex = 16;
+            this.BTN_Ergebnis.Text = "=";
+            this.BTN_Ergebnis.UseVisualStyleBackColor = true;
+            this.BTN_Ergebnis.Click += new System.EventHandler(this.BTN_Ergebnis_Click);
+            // 
+            // BTN_Clear
+            // 
+            this.BTN_Clear.Location = new System.Drawing.Point(208, 146);
+            this.BTN_Clear.Name = "BTN_Clear";
+            this.BTN_Clear.Size = new System.Drawing.Size(50, 50);
+            this.BTN_Clear.TabIndex = 17;
+            this.BTN_Clear.Text = "CR";
+            this.BTN_Clear.UseVisualStyleBackColor = true;
+            this.BTN_Clear.Click += new System.EventHandler(this.BTN_Clear_Click);
+            // 
+            // BTN_Sqrt
+            // 
+            this.BTN_Sqrt.Location = new System.Drawing.Point(208, 202);
+            this.BTN_Sqrt.Name = "BTN_Sqrt";
+            this.BTN_Sqrt.Size = new System.Drawing.Size(50, 50);
+            this.BTN_Sqrt.TabIndex = 18;
+            this.BTN_Sqrt.Text = "√";
+            this.BTN_Sqrt.UseVisualStyleBackColor = true;
+            this.BTN_Sqrt.Click += new System.EventHandler(this.BTN_Sqrt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 450);
+            this.Controls.Add(this.BTN_Sqrt);
+            this.Controls.Add(this.BTN_Clear);
+            this.Controls.Add(this.BTN_Ergebnis);
+            this.Controls.Add(this.BTN_Comma);
+            this.Controls.Add(this.BTN_0);
+            this.Controls.Add(this.BTN_9);
+            this.Controls.Add(this.BTN_8);
+            this.Controls.Add(this.BTN_7);
+            this.Controls.Add(this.BTN_6);
+            this.Controls.Add(this.BTN_5);
+            this.Controls.Add(this.BTN_4);
+            this.Controls.Add(this.BTN_3);
+            this.Controls.Add(this.BTN_2);
+            this.Controls.Add(this.BTN_1);
+            this.Controls.Add(this.BTN_Division);
+            this.Controls.Add(this.BTN_Multiply);
+            this.Controls.Add(this.BTN_Minus);
+            this.Controls.Add(this.BTN_Plus);
+            this.Controls.Add(this.RTB_Ausgabe_Fenster);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox RTB_Ausgabe_Fenster;
+        private System.Windows.Forms.Button BTN_Plus;
+        private System.Windows.Forms.Button BTN_Minus;
+        private System.Windows.Forms.Button BTN_Multiply;
+        private System.Windows.Forms.Button BTN_Division;
+        private System.Windows.Forms.Button BTN_1;
+        private System.Windows.Forms.Button BTN_2;
+        private System.Windows.Forms.Button BTN_3;
+        private System.Windows.Forms.Button BTN_4;
+        private System.Windows.Forms.Button BTN_5;
+        private System.Windows.Forms.Button BTN_6;
+        private System.Windows.Forms.Button BTN_7;
+        private System.Windows.Forms.Button BTN_8;
+        private System.Windows.Forms.Button BTN_9;
+        private System.Windows.Forms.Button BTN_0;
+        private System.Windows.Forms.Button BTN_Comma;
+        private System.Windows.Forms.Button BTN_Ergebnis;
+        private System.Windows.Forms.Button BTN_Clear;
+        private System.Windows.Forms.Button BTN_Sqrt;
+    }
+}
+
---
+++ Rechner v1/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rechner_v1
+{
+    public partial class Form1 : Form
+    {
+        public int vari1, vari2,ergebnisI;
+        public double vard1, vard2,ergebnisD;
+        public string opperator = " ";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_1_Click(object sender, EventArgs e)
+        {
+            RTB_Ausgabe_Fenster.Text += "1";
+        }
+
+        private void BTN_2_Click(object sender, EventArgs e)
+        {
+            RTB_Ausgabe_Fenster.Text += "2";
+        }
+
+        private void BTN_3_Click(object sender, EventArgs e)
+        {
+            RTB_Ausgabe_Fenster.Text += "3";
+        }
+
+        private void BTN_4_Click(object sender, EventArgs e)
+        {
+            RTB_Ausgabe_Fenster.Text += "4";
+        }
+
+        private void BTN_5_Click(object sender, EventArgs e)
+        {
+            RTB_Ausgabe_Fenster.Text += "5";
+        }
+
+        private void BTN_6_Click(object sender, EventArgs e)
+        {
+            RTB_Ausgabe_Fenster.Text += "6";
+        }
+
+        private void BTN_7_Click(object sender, EventArgs e)
+        {
+            RTB_Ausgabe_Fenster.Text += "7";
+        }
+
+        private void BTN_8_Click(object sender, EventArgs e)
+        {
+            RTB_Ausgabe_Fenster.Text += "8";
+        }
+
+        private void BTN_9_Click(object sender, EventArgs e)
+        {
+            RTB_Ausgabe_Fenster.Text += "9";
+        }
+
+        private void BTN_0_Click(object sender, EventArgs e)
+        {
+            RTB_Ausgabe_Fenster.Text += "0";
+        }
+
+        private void BTN_Comma_Click(object sender, EventArgs e)
+        {
+            RTB_Ausgabe_Fenster.Text += ".";
+        }
+
+        private void BTN_Plus_Click(object sender, EventArgs e)
+        {
+            vari1 = (Int32.Parse(RTB_Ausgabe_Fenster.Text));
+            RTB_Ausgabe_Fenster.Clear();
+            opperator = "+";
+        }
+
+        private void BTN_Minus_Click(object sender, EventArgs e)
+        {
+            vari1 = (Int32.Parse(RTB_Ausgabe_Fenster.Text));
+            RTB_Ausgabe_Fenster.Clear();
+            opperator = "-";
+        }
+
+        private void BTN_Multiply_Click(object sender, EventArgs e)
+        {
+            vari1 = (Int32.Parse(RTB_Ausgabe_Fenster.Text));
+            RTB_Ausgabe_Fenster.Clear();
+            opperator = "*";
+        }
+
+        private void BTN_Division_Click(object sender, EventArgs e)
+        {
+            vard1 = (Double.Parse(RTB_Ausgabe_Fenster.Text));
+            RTB_Ausgabe_Fenster.Clear();
+            opperator = "/";
+        }
+
+        private void BTN_Sqrt_Click(object sender, EventArgs e)
+        {
+            vard1 = (Double.Parse(RTB_Ausgabe_Fenster.Text));
+            RTB_Ausgabe_Fenster.Clear();
+            opperator = "sqrt";
+        }
+
+        private void BTN_Ergebnis_Click(object sender, EventArgs e)
+        {
+            if (opperator == "+")
+            {
+                vari2 = (Int32.Parse(RTB_Ausgabe_Fenster.Text));
+                RTB_Ausgabe_Fenster.Clear();
+                ergebnisI = vari1 + vari2;
+                RTB_Ausgabe_Fenster.Text = Convert.ToString(ergebnisI);
+            }
+
+            if (opperator == "-")
+            {
+                vari2 = (Int32.Parse(RTB_Ausgabe_Fenster.Text));
+                RTB_Ausgabe_Fenster.Clear();
+                ergebnisI = vari1 - vari2;
+                RTB_Ausgabe_Fenster.Text = Convert.ToString(ergebnisI);
+            }
+
+            if (opperator == "*")
+            {
+                vari2 = (Int32.Parse(RTB_Ausgabe_Fenster.Text));
+                RTB_Ausgabe_Fenster.Clear();
+                ergebnisI = vari1 * vari2;
+                RTB_Ausgabe_Fenster.Text = Convert.ToString(ergebnisI);
+            }
+
+            if (opperator == "/")
+            {
+                vard2 = (Double.Parse(RTB_Ausgabe_Fenster.Text));
+                RTB_Ausgabe_Fenster.Clear();
+                ergebnisD = vard1 / vard2;
+                RTB_Ausgabe_Fenster.Text = Convert.ToString(ergebnisD);
+            }
+
+            if (opperator == "sqrt")
+            {
+                RTB_Ausgabe_Fenster.Clear();
+                ergebnisD = Math.Sqrt(vard1);
+                RTB_Ausgabe_Fenster.Text = Convert.ToString(ergebnisD);                
+            }
+        }
+
+        private void BTN_Clear_Click(object sender, EventArgs e)
+        {
+            vard1 = 0.0; vard2 = 0.0; ergebnisD = 0.0;
+            vari1 = 0; vari2 = 0; ergebnisI = 0;
+            RTB_Ausgabe_Fenster.Clear();
+        }
+    }
+}
